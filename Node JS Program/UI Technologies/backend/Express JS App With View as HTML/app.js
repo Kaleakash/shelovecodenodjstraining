7581580752eda3adf1e0e76bbdf57dd714093f25
akash <akash300383@gmail.com>
2023-01-16 20:44:04 +0530

    done

--- Node JS Program/UI Technologies/backend/Express JS App With View as HTML/app.js
+++ Node JS Program/UI Technologies/backend/Express JS App With View as HTML/app.js
@@ -1,6 +1,6 @@
-const { response } = require("express");
-let express = require("express");
 
+let express = require("express");
+let fs = require("fs");
 let app = express();
 
 
@@ -30,13 +30,32 @@ app.get("/signIn",(request,response)=> {
     let email = request.query.email;            // receive email field from form 
     let password = request.query.password;         // recevie password field from form 
     console.log(email+" "+password);
-    if(email=="raj@gmail.com" && password=="123"){
+    // if(email=="raj@gmail.com" && password=="123"){
+    //     response.send("successfully login");
+    // }else {
+    //     response.send("Failure try once again");
+    // }
+    let data = fs.readFileSync(__dirname+"\\logins.json");   // read data from file data in the buffer 
+    let login = JSON.parse(data.toString());    // convert that data into string and that string convert into json 
+    if(login.email==email && login.password==password){
         response.send("successfully login");
     }else {
-        response.send("Failure try once again");
+        response.send("failure try once again");
     }
+
     // response.send("done!")
 })
+app.get("/accountCreate",(request,response)=> {
+    let emailValue = request.query.email;            // receive email field from form 
+    let passwordValue = request.query.password;         // recevie password field from form 
+    let login = {email:emailValue,password:passwordValue}; // literal style object
+    console.log(login);
+    fs.writeFileSync("logins.json",JSON.stringify(login));
+    response.send("Account Created...");
+})
 
+app.get("/signUp",(request,response)=> {
+    response.sendFile(__dirname+"\\signUp.html");
+})
 
 app.listen(3000,()=>console.log("server running on port nubmber 3000"));
